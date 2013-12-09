b4c2a59d26698b01fd9f6132cb87262e65b87a00
Dave Laird <kiwiupover@gmail.com>
2013-12-09 11:59:22 -0800

    WIP for the styling the page

--- app/router.js
+++ app/router.js
@@ -1,7 +1,7 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
 Router.map(function() {
-  this.route('forecast', { path: '/forecast/:location_id' });
+  this.resource('forecast', { path: '/forecast/:location_id' });
 });
 
 Router.reopen({
