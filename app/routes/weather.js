99c4a9487ed107b00881abfe69eaeba5fa2ecf6f
Erik Trom <erik.trom@gmail.com>
2013-12-17 00:24:46 -0800

    transitionTo

--- app/routes/weather.js
+++ app/routes/weather.js
@@ -1,13 +1,12 @@
 export default Ember.Route.extend({
   model: function(params) {
-    window.console.log()
-    return {blah: 'hello'};
-    // return this.store.find('weather', params.location_id);
+    window.console.log("we got the right event with the aprams ", params)
+    return this.store.find('weather', params.location_id);
   },
   actions: {
     searchHandler: function (val) {
       window.console.log("the search term from the router is", val);
-      this.transitionTo('weather.search', val);
+      this.transitionTo('/weather/' + encodeURIComponent(val));
     }
   }
 });
