cb1ddd21851590eaf03071e4674e5dbed423fe02
Erik Trom <erik.trom@gmail.com>
2013-12-17 00:12:00 -0800

    outlets, routes oh my

--- app/routes/weather.js
+++ app/routes/weather.js
@@ -1,11 +1,13 @@
 export default Ember.Route.extend({
   model: function(params) {
+    window.console.log()
     return {blah: 'hello'};
     // return this.store.find('weather', params.location_id);
   },
   actions: {
     searchHandler: function (val) {
-      console.log("the search term from the router is", val);
+      window.console.log("the search term from the router is", val);
+      this.transitionTo('weather.search', val);
     }
   }
 });
