74533250e5e0a29c2f00e342af6fb7442e10aa9b
Erik Trom <erik.trom@gmail.com>
2014-01-07 08:36:57 -0800

    - change blur radius - WIP: weather-current component saveLocation action handler - add local storage adapter to bower - remove commented out code

--- app/components/weather-current.js
+++ app/components/weather-current.js
@@ -4,7 +4,14 @@ export default Ember.Component.extend({
   didInsertElement: function(){
     this.$('.bg').css('background-image', 'url(' + this.get('image') + ')');
     this.$('#bg').foggy({
-      blurRadius: 4
+      blurRadius: 10
     });
   },
+
+  actions: {
+    saveLocation: function (location) {
+      window.console.log("who the fuck are you", location);
+      this.sendAction('saveLocationHandler', location);
+    }
+  }
 });
--- app/routes/weather/search.js
+++ app/routes/weather/search.js
@@ -3,6 +3,13 @@ export default Ember.Route.extend({
     var model = this.store.find('weather', params.term);
     window.console.log("from search route %o", model);
     return model;
+  },
+
+  actions: {
+    handleSaveLocation: function (location) {
+      window.alert('hello' + ' ' + location);
+      // this.store.
+    },
   }
 
 });
