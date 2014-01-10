beccda7f05341de35466e2e60a566c5b7ee120f3
Dave Laird <kiwiupover@gmail.com>
2014-01-09 21:34:38 -0800

    fixed a typo

--- app/routes/weather/search.js
+++ app/routes/weather/search.js
@@ -2,7 +2,7 @@ export default Ember.Route.extend({
   model: function (params) {
     var model = this.store.find('weather', params.term);
     window.console.log("from search route %o", model);
-    return {};
+    return model;
   },
 
   actions: {
